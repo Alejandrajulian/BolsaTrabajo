edfbead979138af477a32cdb917f6598249c6904
Gerardo Nava <gerardonavart@outlook.com>
2018-06-01 00:56:01 -0500

    Creación de vistas

---
+++ BolsaDeTrabajo/BolsaDeTrabajo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BolsaDeTrabajo.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BolsaDeTrabajo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnExito.Clicked += (sender, e) =>
+            {
+                Navigation.PushAsync(new Registro());
+            };
+		}
+	}
+}
